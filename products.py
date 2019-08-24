3123fca88b1e910e12d1af59a7db6e66ed994d2e
Vivi <vvw224@gmail.com>
2019-08-24 10:59:13 +0800

    version 1.0

---
+++ products.py
@@ -0,0 +1,16 @@
+
+products = []
+
+while True:
+    name = input('請輸入商品名稱: ')
+    if name == 'q':
+    	break
+    price = input('請輸入商品價格: ')
+    products.append([name, price])
+    #p = []
+    #p.append(name)
+    #p.append(price)
+    #products.append(p)
+print(products)
+
+products[0][0]
